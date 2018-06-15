8dd4e4e2152578a3331420c681ccfcccbd2c7671
polancof1182 <40307510+polancof1182@users.noreply.github.com>
2018-06-15 19:10:57 -0400

    Add files via upload
    First submission

---
+++ P2T1-SalesPrediction_PolancoDelaRosaFrancisco.py
@@ -0,0 +1,14 @@
+# CT1-110
+# P2T1-Sales Prediction
+# Francisco PolancoDelaRosa
+# 6/14/2018
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
